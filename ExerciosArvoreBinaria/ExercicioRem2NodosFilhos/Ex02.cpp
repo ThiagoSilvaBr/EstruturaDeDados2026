62e7224f7d7be66433b2bc3573aeed844f4efdb4
Thiago Henrique <164083408+ThiagoSilvaBr@users.noreply.github.com>
2026-03-30 20:40:13 -0300

    Add files via upload

---
+++ ExerciosArvoreBinaria/ExercicioRem2NodosFilhos/Ex02.cpp
@@ -0,0 +1,5 @@
+/*2. No código, existe a condição if (paiSucessor == filho). Explique, com um
+exemplo de árvore, em qual cenário o sucessor é o filho direto à direita do
+nó a ser removido.*/
+
+//R: Se o sucessor foi filho direto na direita, um exemplo é o próprio exercicio 1 que o 80 é sucessor direto a direita do 70 que foi removido.
